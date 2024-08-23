81b41a61990bc71d7ce71f699b6ae777f57ac74d
allanehp134 <allanehp134@gmail.com>
2024-08-23 10:40:21 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import math
 st.title("Modelo para la deteccion de fraudes por Montos")
 
 # Cargar archivo CSV
-uploaded_file = st.file_uploader("Sube tu archivo CSV", type=["csv"])
+uploaded_file = st.file_uploader("Sube tu archivo CSV", type=["xlsx", "xls"])
 if uploaded_file is not None:
     # Leer el archivo CSV
     df = pd.read_csv(uploaded_file)
